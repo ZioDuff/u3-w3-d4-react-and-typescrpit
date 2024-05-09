e4701c32dff33dccb0600048a1f552f637971086
Jacopo De maio <jajodemaio@gmail.com>
2024-05-09 17:36:18 +0200

    add spinner load at details page

--- src/Components/MyDetails.tsx
+++ src/Components/MyDetails.tsx
@@ -1,60 +1,70 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import {SingleArticle}from "../interfaces/interfaces"
-import { Col, Container, Row } from "react-bootstrap"
+import { SingleArticle } from "../interfaces/interfaces"
+import { Col, Container, Row, Spinner } from "react-bootstrap"
 
-const MyDetails = ()=>{
-    const params = useParams()
-    const artId= params.artId
-    console.log(artId)
+const MyDetails = () => {
+  const params = useParams()
+  const artId = params.artId
+  console.log(artId)
 
-const [singleArt, setSingleArt]= useState<SingleArticle>()
+  const [singleArt, setSingleArt] = useState<SingleArticle>()
+  const [isLoading, setIsLoading] = useState(true)
 
-    const singleFetchArticle = async()=>{
-        try{
-            const resp = await fetch(`https://api.spaceflightnewsapi.net/v4/articles/${artId}`)
-            if(resp.ok){
-                 const singleArt= await resp.json()
-                 setSingleArt(singleArt)
-                 console.log(singleArt)
-            }else{
-                throw new Error("errore nel singolo articolo")
-            }
-            
-        }catch (error){
-            console.log(error)
-        }
+  const singleFetchArticle = async () => {
+    try {
+      const resp = await fetch(
+        `https://api.spaceflightnewsapi.net/v4/articles/${artId}`
+      )
+      if (resp.ok) {
+        const singleArt = await resp.json()
+        setSingleArt(singleArt)
+
+        console.log(singleArt)
+      } else {
+        throw new Error("errore nel singolo articolo")
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
     }
-useEffect(()=>{
+  }
+  useEffect(() => {
     singleFetchArticle()
-},[])
-    return(
-       <Container>
+  }, [])
+  return (
+    <Container>
+      {isLoading && isLoading ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
         <Row>
-            <h1 className="my-4">Grande pagina dettaglio</h1>
-            <Col >
+          <h1 className="my-4">Grande pagina dettaglio</h1>
+          <Col>
             <div className="d-flex gap-3 mb-4">
+              <img
+                className="w-50"
+                src={singleArt?.image_url}
+                alt={singleArt?.title}
+              />
 
-    <img className="w-50" src={singleArt?.image_url} alt={singleArt?.title}/>
-         
-<div className="d-flex flex-column">
-    <h2 className=" mb-auto">{singleArt?.title}</h2>
+              <div className="d-flex flex-column">
+                <h2 className=" mb-auto">{singleArt?.title}</h2>
 
-    <p>{singleArt?.news_site}</p>
-    <p>{singleArt?.published_at}</p>
-    </div>
-
-
-
+                <p>{singleArt?.news_site}</p>
+                <p>{singleArt?.published_at}</p>
+              </div>
             </div>
-    <div>
-        <h4>Summary</h4>
-        <p>{singleArt?.summary}</p>
-        </div>            
-            </Col>
+            <div>
+              <h4>Summary</h4>
+              <p>{singleArt?.summary}</p>
+            </div>
+          </Col>
         </Row>
-       </Container>
-    )
+      )}
+    </Container>
+  )
 }
 export default MyDetails
-
--- src/Components/MyHomePage.tsx
+++ src/Components/MyHomePage.tsx
@@ -1,49 +1,41 @@
 import { useEffect, useState } from "react"
-import {  Container, Row } from "react-bootstrap"
-import {Articles}from "../interfaces/interfaces"
+import { Container, Row } from "react-bootstrap"
+import { Articles } from "../interfaces/interfaces"
 import MyArticleCard from "./MyArticleCard"
 
+const MyHomePage = () => {
+  const [articles, setArticles] = useState<Articles | null>(null)
 
-
-
-const MyHomePage=()=>{
-
-const [articles, setArticles]=useState<Articles>()
-
-const fetchArticles = async()=>{
-    try{
-        const response= await fetch("https://api.spaceflightnewsapi.net/v4/articles")
-        if(response.ok){
-            const articlesObj= await response.json()
-            setArticles(articlesObj)
-           
-        }else{
-            throw new Error("errore nella fetch")
-        }
-
-    } catch(error){
-        console.log(error)
+  const fetchArticles = async () => {
+    try {
+      const response = await fetch(
+        "https://api.spaceflightnewsapi.net/v4/articles"
+      )
+      if (response.ok) {
+        const articlesObj = await response.json()
+        setArticles(articlesObj)
+      } else {
+        throw new Error("errore nella fetch")
+      }
+    } catch (error) {
+      console.log(error)
     }
-}
+  }
 
-useEffect(()=>{
+  useEffect(() => {
     fetchArticles()
-}, [])
+  }, [])
 
-return(
+  return (
     <Container>
-        <Row>
-            <h1 className="my-4">Il mio grande sito di Articoli </h1>
-            {articles?.results.map((art)=>{
-                
-                return(
-                   
-                    <MyArticleCard key={art.id} art={art} />                
-                
-                )
-            })}
-        </Row>
+      <Row>
+        <h1 className="my-4">Il mio grande sito di Articoli </h1>
+        {articles &&
+          articles?.results.map((art) => {
+            return <MyArticleCard key={art.id} art={art} />
+          })}
+      </Row>
     </Container>
-)
+  )
 }
 export default MyHomePage
